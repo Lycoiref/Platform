7b3704abbf62ffe7c84d19f42771dbd4375a89c0
Lycoiref <lycoiref@qq.com>
2024-11-25 00:35:42 +0800

    feat: 完成auth接口login & register

--- server/node/src/routes/api/index.ts
+++ server/node/src/routes/api/index.ts
@@ -1,4 +1,5 @@
 import Router from '@koa/router'
+import auth from './auth'
 import { Context } from 'koa'
 
 const router = new Router()
@@ -8,34 +9,6 @@ interface LoginRequest {
 	password: string
 }
 
-router.post('/login', (ctx) => {
-	const { username, password } = ctx.request.body as LoginRequest
-
-	console.log('[Login] ', username, password)
-
-	const user = ctx.prisma.user.findFirst({
-		where: {
-			username,
-			password,
-		},
-	})
-
-	if (!user) {
-		ctx.response.body = {
-			code: 400,
-			message: '用户名或密码错误',
-		}
-
-		return
-	}
-
-	ctx.response.body = {
-		code: 200,
-		message: '登录成功',
-		data: {
-			token: 'token',
-		},
-	}
-})
+router.use('/auth', auth.routes())
 
 export default router
---
+++ server/node/src/utils/jwt.ts
@@ -0,0 +1,17 @@
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+export const generateToken = (
+	payload: object,
+	expiresIn: string = '1h'
+): string => {
+	return jwt.sign(payload, process.env.SECRET_KEY ?? '', { expiresIn })
+}
+
+export const verifyToken = (token: string): string | JwtPayload => {
+	try {
+		return jwt.verify(token, process.env.SECRET_KEY ?? '')
+	} catch (err) {
+		console.error('Invalid token:', err)
+		return ''
+	}
+}
